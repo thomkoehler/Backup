e1976367cb463f69675fff080d5ce4694ebda190
thomkoehler <thom.koehler@gmx.com>
2014-08-15 09:55:06 +0200

    Backup

--- src/Compressor.hs
+++ src/Compressor.hs
@@ -12,26 +12,30 @@ import Text.Printf(printf)
 
 -----------------------------------------------------------------------------------------------------------------------
 
+compress  :: [String] -> String -> IO ()
+compress = compress_rar 
+
+
+compress_rar :: [String] -> String -> IO ()
+compress_rar fileList archiveName =
+   withListFile fileList $ \fileListName -> do
+      let cmd = printf "rar a \"%s\" \"@%s\" " archiveName fileListName
+      system' cmd
+
+
 withListFile :: [String] -> (FilePath -> IO a) -> IO a
 withListFile fileList listFun = 
    withSystemTempFile "Backup" $ \filePath handle -> do
       forM_ fileList (hPutStrLn handle)
       hClose handle
       listFun filePath
+      
+      
+system' :: String -> IO ()
+system' cmd = do
+   res <- system cmd
+   case res of
+      ExitSuccess   -> return ()
+      ExitFailure _ -> error $ printf "Command %s failed." cmd
 
-
-compress  :: [String] -> String -> IO ()
-compress = compress_7z 
-
-  
-compress_7z :: [String] -> String -> IO ()
-compress_7z fileList fileName =
-   withListFile fileList $ \fileListName -> do
-      let cmd = printf "7z.exe a  -r \"-i@%s\" %s" fileListName fileName
-      print cmd
-      res <- system cmd
-      case res of
-         ExitSuccess   -> return ()
-         ExitFailure _ -> error $ printf "Command %s failed." cmd     
-   
 -----------------------------------------------------------------------------------------------------------------------
