d375c6efe6be6c8c5cec28a11fb5276082fb3026
thomkoehler <thom.koehler@gmx.com>
2014-08-15 13:39:06 +0200

    Backup

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,17 @@
 
 -----------------------------------------------------------------------------------------------------------------------
 
-module Main where
+module Main(main) where
 
 import System.Environment(getArgs)
+import Text.Printf(printf)
+import Control.Monad(forM_)
+import Control.Lens
 
 import Options
 import BackupSuite
+import DirScanner
+import Compressor
 
 -----------------------------------------------------------------------------------------------------------------------
 
@@ -15,7 +20,28 @@ main = do
    argv <- getArgs
    opts <- getOptions argv
    suites <- decodeFile $ optInput opts
-   print suites
+   forM_ suites doBackupSuite 
    return ()
+   
+
+doBackupSuite :: BackupSuite -> IO ()
+doBackupSuite suite = do
+   let targetDir = suite ^. bsDir
+   forM_ (suite ^. bsBackups) $ doBackup targetDir 
+
+
+doBackup :: FilePath -> Backup -> IO ()
+doBackup targetDir backup = 
+   if backup ^. bEnabled
+      then do
+         fileList <- list backup
+         if null fileList 
+            then 
+               printf "Backup %s is empty." $ backup ^. bName
+            else
+               compress fileList (targetDir ++ "/" ++ (backup ^. bName))
+      else
+         printf "Backup %s is diabled." $ backup ^. bName       
+
 
 -----------------------------------------------------------------------------------------------------------------------
